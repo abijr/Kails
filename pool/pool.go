ce6d5752245020087ccc4146139638c48578ed17
abijr <abijr.mtz@gmail.com>
2014-04-03 21:23:16 -0600

    Add pool.go

---
+++ pool/pool.go
@@ -0,0 +1,26 @@
+//wsrs is a simple spaced repetion web application.
+package pool
+
+import (
+	"github.com/stathat/spitz"
+	"io"
+)
+
+var pool *spitz.Pool
+
+func init() {
+	pool = spitz.New("/home/potemkin/Projects/go/src/bitbucket.com/abijr/wsrs/templates", true)
+	err := pool.RegisterLayout("main", "main/header", "main/footer", "", "")
+	if err != nil {
+		panic(err)
+	}
+
+	err = pool.Register("main/index", "", "")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Render(layout string, template string, data interface{}, writer io.Writer) error {
+	return pool.Render(layout, template, data, writer)
+}
