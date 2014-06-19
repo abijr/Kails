5ed513a1b1ac97988b5d6105bbf84a412bbc1849
abijr <abijr.mtz@gmail.com>
2014-06-19 00:54:38 -0600

    Setup basic functionality for working with mongodb

--- db/db.go
+++ db/db.go
@@ -3,5 +3,26 @@ package db
 import "labix.org/v2/mgo"
 
 var (
-	db mgo.Session
+	db *mgo.Session
 )
+
+func init() {
+	Init()
+}
+
+func Init() {
+	var err error
+	db, err = mgo.Dial("localhost:" + "27017")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Close() {
+	db.Close()
+}
+
+func Collection(collection string) *mgo.Collection {
+	c := db.DB("kails").C(collection)
+	return c
+}
