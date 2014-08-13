d3777977252acc448bc684ca592467788d0dd8a1
abijr <abijr.mtz@gmail.com>
2014-08-13 12:01:29 -0600

    Setup login and home routes

--- routes/user.go
+++ routes/user.go
@@ -1,13 +1,23 @@
 package routes
 
 import (
+	"log"
+
 	"bitbucket.com/abijr/kails/middleware"
 	"bitbucket.com/abijr/kails/models"
 )
 
 func Home(ctx *middleware.Context) {
-	ctx.Data["Title"] = "Welcome"
-	ctx.HTML(200, "main/main")
+	if ctx.IsLogged {
+		ctx.Data["Title"] = "Home"
+		ctx.Data["Name"] = ctx.User.Username
+		ctx.Data["Email"] = ctx.User.Email
+		ctx.Data["Since"] = ctx.User.Created.Format("January 21 of 2009")
+		ctx.HTML(200, "user/home")
+	} else {
+		ctx.Data["Title"] = "Welcome"
+		ctx.HTML(200, "main/main")
+	}
 }
 
 func SignUp(ctx *middleware.Context) {
@@ -26,11 +36,26 @@ func SignUpPost(ctx *middleware.Context, form models.UserSignupForm) {
 }
 
 func Login(ctx *middleware.Context) {
-	ctx.Data["Title"] = "Sign In"
-	ctx.HTML(200, "user/login")
+	if ctx.IsLogged {
+		ctx.Redirect("/")
+	} else {
+		ctx.Data["Title"] = "Login"
+		ctx.HTML(200, "user/login")
+	}
 }
 
 func LoginPost(ctx *middleware.Context, form models.UserLoginForm) {
-	ctx.Data["Title"] = "Sign In"
-	ctx.HTML(200, "user/login")
+	ctx.Data["Title"] = "Home"
+	user, err := models.UserByEmail(form.Email)
+	if err != nil {
+		// TODO: fill this up.
+		log.Println(err)
+		log.Println(user)
+	}
+
+	ctx.User = *user
+	ctx.Session.Set("name", user.Username)
+	ctx.IsLogged = true
+
+	ctx.Redirect("/")
 }
