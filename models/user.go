2e6bd57df7e038dfec14b67873960246dd2e042f
abijr <abijr.mtz@gmail.com>
2014-08-12 18:33:35 -0600

    Separated user form models from user models.

--- models/user.go
+++ models/user.go
@@ -2,22 +2,17 @@ package models
 
 import (
 	"errors"
-	"net/http"
 	"regexp"
-	"strings"
 	"time"
 
-	"github.com/martini-contrib/binding"
-
 	"log"
-
-	"labix.org/v2/mgo/bson"
 
 	"bitbucket.com/abijr/kails/db"
 	"bitbucket.com/abijr/kails/util"
 )
 
 const (
+	// Name of the database collection holding user information
 	UserCollection = "users"
 )
 
@@ -37,15 +32,6 @@ type User struct {
 	Levels   []Level   "levels"
 }
 
-/* User forms */
-
-// User signup form
-type UserSignupForm struct {
-	Username string `bson:"name" form:"username" binding:"required"`
-	Email    string `bson:"email" form:"email" binding:"required"`
-	Password string `bson:"pass" form:"password" binding:"required"`
-}
-
 // Utility variables
 var (
 	// users collection
@@ -54,69 +40,9 @@ var (
 	// email regexp
 	emailPattern = regexp.MustCompile("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$")
 
-	ErrUserNotExist    = errors.New("User does not exist")
-	ErrUserNameIllegal = errors.New("User name contains illegal characters")
+	errUserNotExist    = errors.New("user does not exist")
+	errUserNameIllegal = errors.New("user name contains illegal characters")
 )
-
-// Validation Errors
-var (
-	ErrUserAlreadyExist = binding.Error{
-		FieldNames:     []string{"username"},
-		Classification: "DuplicateError",
-		Message:        "Username is already in use",
-	}
-	ErrEmailAlreadyExist = binding.Error{
-		FieldNames:     []string{"email"},
-		Classification: "DuplicateError",
-		Message:        "Email is already in use",
-	}
-	ErrEmailInvalid = binding.Error{
-		FieldNames:     []string{"email"},
-		Classification: "InvalidError",
-		Message:        "Email is invalid",
-	}
-	ErrPasswordTooShort = binding.Error{
-		FieldNames:     []string{"password"},
-		Classification: "PasswordError",
-		Message:        "Password too short, must be at least 8 characters long",
-	}
-)
-
-func (uf UserSignupForm) Validate(errors binding.Errors, req *http.Request) binding.Errors {
-
-	uf.Username = strings.TrimSpace(uf.Username)
-	uf.Email = strings.TrimSpace(uf.Email)
-
-	query := bson.M{
-		"$or": [2]bson.M{
-			bson.M{"name": uf.Username},
-			bson.M{"email": uf.Email},
-		},
-	}
-
-	// Find if user or email already in db
-	docs := users.
-		Find(query).
-		Select(bson.M{"name": 1, "email": 1}).
-		Limit(2).
-		Iter()
-
-	var result User
-	for docs.Next(&result) {
-		if uf.Username == result.Username {
-			errors = append(errors, ErrUserAlreadyExist)
-		}
-
-		if uf.Email == result.Email {
-			errors = append(errors, ErrEmailAlreadyExist)
-		}
-	}
-
-	if len(uf.Password) < 8 {
-		errors = append(errors, ErrPasswordTooShort)
-	}
-	return errors
-}
 
 func NewUser(uf UserSignupForm) error {
 	salt, _ := util.NewSalt()
@@ -138,14 +64,6 @@ func NewUser(uf UserSignupForm) error {
 	return nil
 }
 
-func AuthUser(uf UserLoginForm) {
-	var user &User
-	users.Find(uf.Email).One(user)
-	
-
-
-}
-
 func UserByName(name string) (*User, error) {
 	var user *User
 
@@ -154,9 +72,10 @@ func UserByName(name string) (*User, error) {
 		user = new(User)
 		user.Username = name
 	} else {
-		return nil, ErrUserNotExist
+		return nil, errUserNotExist
 	}
 
+	// TODO: Error check user find here
 	users.Find(user.Username).One(user)
 	return user, nil
 }
---
+++ models/user_forms.go
@@ -0,0 +1,117 @@
+package models
+
+import (
+	"net/http"
+	"strings"
+
+	"log"
+
+	"bitbucket.com/abijr/kails/util"
+	"github.com/martini-contrib/binding"
+	"labix.org/v2/mgo/bson"
+)
+
+/* User forms */
+
+// User signup form
+type UserSignupForm struct {
+	Username string `bson:"name" form:"username" binding:"required"`
+	Email    string `bson:"email" form:"email" binding:"required"`
+	Password string `bson:"pass" form:"password" binding:"required"`
+}
+
+// User login form
+type UserLoginForm struct {
+	Email    string `bson:"email" form:"email" binding:"required"`
+	Password string `bson:"pass" form:"password" binding:"required"`
+}
+
+// Validation errors
+var (
+	errUserAlreadyExist = binding.Error{
+		FieldNames:     []string{"username"},
+		Classification: "DuplicateError",
+		Message:        "Username is already in use",
+	}
+	errEmailAlreadyExist = binding.Error{
+		FieldNames:     []string{"email"},
+		Classification: "DuplicateError",
+		Message:        "Email is already in use",
+	}
+	errEmailInvalid = binding.Error{
+		FieldNames:     []string{"email"},
+		Classification: "InvalidError",
+		Message:        "Email is invalid",
+	}
+	errPasswordTooShort = binding.Error{
+		FieldNames:     []string{"password"},
+		Classification: "PasswordError",
+		Message:        "Password too short, must be at least 8 characters long",
+	}
+
+	errWrongEmailOrPassword = binding.Error{
+		FieldNames:     []string{"password", "username"},
+		Classification: "AuthError",
+		Message:        "Wrong username or password",
+	}
+)
+
+func (uf UserSignupForm) Validate(errors binding.Errors, req *http.Request) binding.Errors {
+
+	uf.Username = strings.TrimSpace(uf.Username)
+	uf.Email = strings.TrimSpace(uf.Email)
+
+	query := bson.M{
+		"$or": [2]bson.M{
+			bson.M{"name": uf.Username},
+			bson.M{"email": uf.Email},
+		},
+	}
+
+	// Find if user or email already in db
+	docs := users.
+		Find(query).
+		Select(bson.M{"name": 1, "email": 1}).
+		Limit(2).
+		Iter()
+
+	var result User
+	for docs.Next(&result) {
+		if uf.Username == result.Username {
+			errors = append(errors, errUserAlreadyExist)
+		}
+
+		if uf.Email == result.Email {
+			errors = append(errors, errEmailAlreadyExist)
+		}
+	}
+
+	if len(uf.Password) < 8 {
+		errors = append(errors, errPasswordTooShort)
+	}
+	return errors
+}
+
+// Validate checks if user exists and if password correct returns same error for all errors
+func (ulif UserLoginForm) Validate(errors binding.Errors, req *http.Request) binding.Errors {
+	var user User
+
+	// Get user password
+	err := users.
+		Find(bson.M{"email": ulif.Email}).
+		Select(bson.M{"pass": 1}).
+		One(&user)
+
+	if err != nil {
+		log.Println(err)
+		errors = append(errors, errWrongEmailOrPassword)
+		return errors
+	}
+	inputPassword := util.HashPassword(ulif.Password, user.Salt)
+
+	if cmp := util.PasswordCompare(inputPassword, user.Password); cmp != 1 {
+		errors = append(errors, errWrongEmailOrPassword)
+	}
+
+	return errors
+}
