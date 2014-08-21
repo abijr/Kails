c2eae278d150dee0ed01da99e0187b880f16975e
abijr <abijr.mtz@gmail.com>
2014-08-20 23:43:47 -0600

    :memo:

--- db/db.go
+++ db/db.go
@@ -10,18 +10,22 @@ func init() {
 	Init()
 }
 
+// Init starts connection to mongodb server
 func Init() {
 	var err error
+	// Need to make this configurable somehow...
 	db, err = mgo.Dial("localhost:" + "27017")
 	if err != nil {
 		panic(err)
 	}
 }
 
+// Close closes database connection
 func Close() {
 	db.Close()
 }
 
+// Collection returns a pointer to the named collection
 func Collection(collection string) *mgo.Collection {
 	c := db.DB("kails").C(collection)
 	return c
