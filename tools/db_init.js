438abf3fa9ee30959fcbce654b59ef62e0fd0b8f
abijr <abijr.mtz@gmail.com>
2014-08-20 23:32:50 -0600

    Updated database schema, it now includes levels and vocabulary. Also includes their respective ensureindex operations

--- tools/db_init.js
+++ tools/db_init.js
@@ -4,15 +4,59 @@
 
 use kails;
 
+// User = user
+// Password = password
+default_user = {
+	"name" : "user",
+	"email" : "user@email.com",
+	"pass" : BinData(0,"RurG60nC/Kx9N0MumOq74K7tNwebAjWC9AYXhJOqTFY="),
+	"salt" : BinData(0,"Li6QELxiH4vcqg=="),
+	"lang" : "",
+	"since" : ISODate("2014-08-14T20:28:00.414Z"),
+	"levels" : [ ],
+};
+db.users.save(default_user);
+
+// English language collection
+// contains levels and words
+level_1 = {
+	"level": 1,
+	"type": "level",
+	"name": "some sort of description",
+	"version": 1,
+	"word1": {
+		"translation": "blah blah blah",
+		"type": "verb",
+		"sentences": [
+			"english sentence": "translation",
+			"other sentence": "with translation..."
+		]
+	},
+	"word2": "same as above....",
+	"word3": "etc...",
+};
+db.english.save(level_1);
+
+word1 = {
+	"word": "word1",
+	"type": "word",
+	"level": 1,
+	"type": "verb",
+	"translation": "blah blah blah blah",
+	"sentences": [
+		"english sentence": "translation",
+		"other sentence": "translation..."
+	],
+
+};
+db.english.save(word1);
+
+// Add indexes.
+// user collection indexes
 db.users.ensureIndex({"name": 1}, {"unique": true});
 db.users.ensureIndex({"email": 1}, {"unique": true});
 
-default_user = {
-    name: "default_user",
-    email: "default@email.com",
-    pass: "hashed_password",
-    salt: "salt_for_hash"
-    lang: "spanish",
-    since: new Date(),
-    levels: {},
-}
+// english collection indexes
+db.english.ensureIndex({"level": 1});
+db.english.ensureIndex({"type": 1});
+db.english.ensureIndex({"word": 1}, {"sparse": true});
