1059358b726045bcf35a54623c9ef96c74dd714e
abijr <abijr.mtz@gmail.com>
2014-09-03 21:57:20 -0600

    :lipstick: beautified html and js code for Gruntfile and test.html

--- webapp/Gruntfile.js
+++ webapp/Gruntfile.js
@@ -1,59 +1,72 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 
-    sass: {
-      options: {
-        includePaths: ['bower_components/foundation/scss']
-      },
-      dist: {
-        options: {
-          outputStyle: 'compressed'
-        },
-        files: {
-          'dist/css/app.css': 'scss/app.scss'
-        }
-      }
-    },
+		// Esta operacion genera el css desde lo que se encuentra
+		// en el archivo scss/app.scss
+		sass: {
+			options: {
+				includePaths: ['bower_components/foundation/scss']
+			},
+			dist: {
+				options: {
+					outputStyle: 'compressed'
+				},
+				files: {
+					'dist/css/app.css': 'scss/app.scss'
+				}
+			}
+		},
 
-    copy: {
-        js: {
-            expand: true,
-            cwd: 'bower_components/',
-            src: [
-                'jquery/dist/jquery.min.js',
-                'foundation/js/foundation.min.js',
-                'modernizr/modernizr.js',
-                'angular/angular.min.js',
-                ],
-            dest: 'dist/js/',
-            flatten: true,
-            filter: 'isFile',
-        }
-    },
+		// Copia librerias de javascript (manejadas por bower)
+		// a el directorio dist/js
+		copy: {
+			js: {
+				expand: true,
+				cwd: 'bower_components/',
+				src: [
+					'jquery/dist/jquery.min.js',
+					'foundation/js/foundation.min.js',
+					'modernizr/modernizr.js',
+					'angular/angular.min.js',
+				],
+				dest: 'dist/js/',
+				flatten: true,
+				filter: 'isFile',
+			}
+		},
 
-    concat: {
-        study: {
-            src: ['js/study/study.js','js/study/*/*.js'],
-            dest: 'dist/js/study_app.js'
-        }
-    },
+		// Genera un solo archivo conteniendo la aplicacion
+		// de `study`
+		concat: {
+			study: {
+				src: ['js/study/study.js', 'js/study/*/*.js'],
+				dest: 'dist/js/study_app.js'
+			}
+		},
 
-    watch: {
-        grunt: { files: ['Gruntfile.js'] },
+		watch: {
+			grunt: {
+				files: ['Gruntfile.js']
+			},
 
-        sass: {
-            files: 'scss/**/*.scss',
-            tasks: ['sass']
-      }
-    }
-  });
+			sass: {
+				files: 'scss/**/*.scss',
+				tasks: ['sass']
+			},
 
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-concat');
+			study: {
+				files: 'js/study/**/*.js',
+				tasks: ['concat:study']
+			}
+		}
+	});
 
-  grunt.registerTask('build', ['sass','copy','concat']);
-  grunt.registerTask('default', ['build','watch']);
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+
+	grunt.registerTask('build', ['sass', 'copy', 'concat']);
+	grunt.registerTask('default', ['build', 'watch']);
 }
