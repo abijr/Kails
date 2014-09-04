98e194226edbf84cd88c57447cc72feb6ff649ba
abijr <abijr.mtz@gmail.com>
2014-09-03 23:19:58 -0600

    Study app now shows pre-lesson and post-lesson banners.

--- webapp/js/study/controllers/LessonController.js
+++ webapp/js/study/controllers/LessonController.js
@@ -1,23 +1,44 @@
 angular.module('KailsApp')
-    .controller('LessonController', function($scope, $http){
-        var Data;
-        var Counter = 0;
+	.controller('LessonController', function($scope, $http) {
+		var Data;
+		var Counter = 0;
 
-        var NextCard = function () {
-            $scope.Card = Data[Counter].Sentence;
-            width = (100 * (Counter + 1) / Data.length).toString() + "%";
-            $scope.Width = {"width": width}
-            Counter += 1;
-        }
+		$scope.InStart = true;
+		$scope.InLesson = false;
+		$scope.InAfterLesson = false;
 
-        $http.get('study/1').success(function(data){
-            // console.log(data)
-            Data = data;
-            NextCard();
-        });
+		var NextCard = function() {
 
-        $scope.checkAnswer = function() {
-            NextCard();
-        }
-    }
-);
+			if (Counter == Data.length) {
+				$scope.InLesson = false;
+				$scope.InAfterLesson = true;
+			}
+
+			$scope.Card = Data[Counter].Sentence;
+
+			// width in the form of percentage.
+			width = (100 * (Counter + 1) / Data.length).toString() + "%";
+
+			// Width is the style binding for the progress bar.
+			$scope.Width = {
+				"width": width
+			}
+
+			Counter += 1;
+		}
+
+		$http.get('study/1').success(function(data) {
+			// console.log(data)
+			Data = data;
+		});
+
+		$scope.checkAnswer = function() {
+			NextCard();
+		}
+
+		$scope.startLesson = function() {
+			$scope.InLesson = true;
+			$scope.InStart = false;
+			NextCard();
+		}
+	});
