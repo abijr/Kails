fc8df0297c77a98db0649dba9896b1c87267897d
abijr <abijr.mtz@gmail.com>
2014-09-12 12:00:57 -0600

    Added 'study' route to webapp

--- webapp/js/study/study.js
+++ webapp/js/study/study.js
@@ -2,7 +2,10 @@ angular.module('KailsApp', ['ngRoute', 'ngResource'])
 	.config(function($routeProvider, $locationProvider) {
 		$routeProvider.
 		when('/', {
-			templateUrl: '/study',
+			templateUrl: 'partial/program',
+		})
+		.when('/study/:LessonId', {
+			templateUrl: 'partial/study',
+			controller: 'LessonController'
 		});
 	});
-	
