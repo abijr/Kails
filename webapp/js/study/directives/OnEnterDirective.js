2768ee6b4598e49824f17a7a03637e1252a12bf7
abijr <abijr.mtz@gmail.com>
2014-09-26 18:24:00 -0600

    On-Enter directive for angular app

---
+++ webapp/js/study/directives/OnEnterDirective.js
@@ -0,0 +1,14 @@
+angular.module('KailsApp')
+    .directive('onEnter', function() {
+        return function(scope, element, attrs) {
+            element.bind("keydown keypress", function(event) {
+                if(event.which === 13) {
+                    scope.$apply(function(){
+                        scope.$eval(attrs.onEnter, {'event': event});
+                    });
+
+                    event.preventDefault();
+                }
+            });
+        };
+    });
