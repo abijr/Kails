593dc49e6abfd7daa87e8a88c9221342b7528136
aaron <aaron@aaron-GT5654.(none)>
2014-10-09 22:32:34 -0600

    Added showTopics() to PracticeController

--- webapp/js/webrtc/controllers/PracticeController.js
+++ webapp/js/webrtc/controllers/PracticeController.js
@@ -1,9 +1,51 @@
+level_1 = {
+	"Level": 1,
+	"Topics": 4,
+	"Friends": 5,
+	"Features": ["chat"],
+	"Time": 10 //in minutes
+};
+
 angular.module('KailsApp')
-	.controller("InfoController", function($scope, $routeParams, User) {
+	.controller("PracticeController", function($scope, $routeParams, User) {	
+		var add = true;	
+		var topics = ["sports", "entertainment", "vehicles", "various"];
+
+		$scope.topicsAvailable = [];
 		$scope.pageID = "practicePage";
 
-		User.get({name: 'user'}, function (info) {			
+		User.get({name: 'user'}, function (info) {
 			$scope.topics = info.Topics;	
-			console.log($scope.topics);					
+			console.log($scope.topics);				
 		});
+
+		$scope.showTopics = function() {
+			var numTopicsAllowed = level_1.Topics;
+			var currentNumTopics = $scope.topics.length;
+			
+			if(numTopicsAllowed > currentNumTopics) {
+				$scope.add = true;
+
+				if(currentNumTopics > 0) {
+					for(var i = 0; i < topics.length; i++) {
+						for(var j = 0; j < currentNumTopics; j++) {
+							if(topics[i] == $scope.topics[j]) {
+								add = false;
+							}
+						}
+
+						if(add) {
+							$scope.topicsAvailable.push(topics[i]);
+						}
+						add = true;
+					}
+				}
+				else {
+					$scope.topicsAvailable = topics;
+				}
+			}
+			else {
+				alert("You are not allowed to add more topics");
+			}
+		}
 	});
