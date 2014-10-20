1cbf66f6e1f4870454744a89e68842ad728fbb3d
star-lord <aaron_4692@hotmail.com>
2014-10-19 23:38:23 -0600

    Created function to gathered all friend info in 1 array

--- webapp/js/webrtc/controllers/FriendsController.js
+++ webapp/js/webrtc/controllers/FriendsController.js
@@ -1,15 +1,52 @@
 angular.module('KailsApp')
 	.controller('FriendsController', function($scope, Friends) {
 		$scope.pageID = "practicePage";
-		$scope.friends = ["other", "another"]
+		$scope.friends = []
 
-		/*Friends.get({user: 'user'}, function(friends) {
-			//console.log($scope.friends);
+		var topPosition = 10;
+		var leftPosition = 10;
+		var positions = {};
+		var fiendInfo = [];
+		var len = 0;
+
+
+		Friends.get({user: 'user'}, function(info) {
+			console.log(info);
+			getFriendInfo(info);
+		});
+
+		/*$scope.$on("$viewContentLoaded", function() {
+			checkStatus();
 		});*/
 
-		$scope.$on("$viewContentLoaded", function() {
-			checkStatus();
-		});
+		getPositions = function() {
+			var topValue = topPosition;
+			var leftValue = leftValue;
+
+			if(topPosition >= 70) {
+				topPosition += 15;
+				leftPosition = 10;
+			} else {
+				leftPosition += 30;
+			}
+
+			return {
+				top: topValue,
+				left: leftValue
+			}
+		}
+
+		getFriendInfo = function(friendInfo) {
+			len = friendInfo.length;
+
+			for(var i = 0; i < len; i++) {
+				$scope.friends[i].Username = friendInfo[i].Username;
+				$scope.friends[i].StudyLanguage = friendInfo[i].StudyLanguage;
+				positions = getPositions();
+				$scope.friends[i].top = positions.top;
+				$scope.friends[i].left = positions.left;
+			}
+		}
 
 		checkStatus = function() {
 			console.log("enter function");
