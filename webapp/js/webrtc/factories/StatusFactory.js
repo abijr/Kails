ffc0a680e763936363ccab1cb49c24f43f422a3f
star-lord <aaron_4692@hotmail.com>
2014-10-23 22:15:58 -0600

    Added factory to get users connected

--- webapp/js/webrtc/factories/StatusFactory.js
+++ webapp/js/webrtc/factories/StatusFactory.js
@@ -2,3 +2,11 @@ angular.module('KailsApp')
 	.factory("Status", function($resource) {
 		return $resource("/friends/:user/:topic", null, null);
 	})
+	.factory("Connected", function($resource) {
+		return $resource("/friends/connected", null, {
+			'get': {
+				method: "GET",
+				isArray: true
+			}
+		});
+	})
