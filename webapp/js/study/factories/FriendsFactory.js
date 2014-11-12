be8169f52489e3de23c6446b0339953d80343b8a
abijr <abijr.mtz@gmail.com>
2014-11-12 01:35:59 -0700

    Removed old code

--- webapp/js/study/factories/FriendsFactory.js
+++
@@ -1,9 +0,0 @@
-angular.module('KailsApp')
-	.factory("Friends", function($resource) {
-        return $resource('/webapp/friends/:user', null, {
-            'get': {
-                method: "GET",
-                isArray: true
-            }
-        });
-	})
